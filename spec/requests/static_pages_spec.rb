e7b633b42620af83ac12ea039e98d87dbdf161ac
Diogo Folque Teixeira <folque@gmail.com>
2014-07-11 01:33:49 +0200

    Testing static content

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,6 +7,24 @@ describe "Static pages" do
     it "should have the content 'Ruby On Flowers'" do
       visit '/static_pages/home'
       expect(page).to have_content('Ruby On Flowers')
+      expect(page).to have_title('lala | Flowers')
     end
   end
+  
+  describe "Help page" do
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
+      expect(page).to have_title('lala | Help')
+    end
+  end
+  
+  describe "About page" do
+    it "should have the content 'About us'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About us')
+      expect(page).to have_title('lala | About')
+    end
+  end
+  
 end

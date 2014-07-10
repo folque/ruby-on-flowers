a92ef32359b4786c055d69300c23a2620ad7bcf7
Diogo Folque Teixeira <folque@gmail.com>
2014-07-10 22:33:27 +0200

    config changes for heroku 2

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,4 @@
-RubyOnFlowers::application.configure do
+RubyOnFlowers::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on

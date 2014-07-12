1fa922f3df551f6b8e5fa15aba9aaff926842267
Diogo Folque Teixeira <folque@gmail.com>
2014-07-12 05:00:38 +0200

    before starting layouts

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,10 +4,10 @@ describe "Static pages" do
   
   describe "Home page" do
     
-    it "should have the content 'Ruby On Flowers'" do
+    it "shouldn't have the content 'Home'" do
       visit '/static_pages/home'
       expect(page).to have_content('Ruby On Flowers')
-      expect(page).to have_title('lala | Flowers')
+      expect(page).not_to have_title('| Home')
     end
   end
   
@@ -15,7 +15,7 @@ describe "Static pages" do
     it "should have the content 'Help'" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
-      expect(page).to have_title('lala | Help')
+      expect(page).to have_title('| Help')
     end
   end
   
@@ -23,14 +23,14 @@ describe "Static pages" do
     it "should have the content 'About us'" do
       visit '/static_pages/about'
       expect(page).to have_content('About us')
-      expect(page).to have_title('lala | About')
+      expect(page).to have_title('| About')
     end
   end
 
   describe "Contact page" do
     it "should have the content 'Contact'" do
       visit '/static_pages/contact'
-      expect(page).to have_title('Contact')
+      expect(page).to have_title('| Contact')
     end
   end
   
